91b4bb54c66204f2592e8d3b6dbe9f6cd1022731
AliHamed-10003002 <ali.hamed@student.giu-uni.de>
2025-11-11 09:42:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,38 @@ using namespace std;
 
 
 // ==================== Instructor Class Implementation ====================
+class instructor : public person{
+private:
+string department;
+int experienceYears;
+public:
+instructor(string dep, int expyears, string name, int id) : Person(name,id){
+department = dep;
+experienceYears = expyears;
+}
 
+instructor(){
+department = "";
+experienceYears = 0;
+name = "";
+id = -1;
+}
+
+string getDept(){
+  return department;
+}
+int getExpYears(){
+  return experienceYears;
+}
+
+
+~instructor(){}
+  
+void display(){
+  Person.display();
+cout<<"Instructor's department: "<<getDept()<<" Instructor years of experience: "<<getExpYears()<<endl;
+}
+}
 
 
 
