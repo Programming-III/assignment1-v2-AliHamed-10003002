2516ee00a346387d425d8f7f4964222d5c6458d3
AliHamed-10003002 <ali.hamed@student.giu-uni.de>
2025-11-11 09:19:03 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,7 +5,30 @@
 #include "Student.h"
 using namespace std;
 class course{
-string 
+private:
+string coursecode;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
+
+public:
+course(string coursecode, string courseName, int maxStudents, Student* students, int currentStudents){
+this-> coursecode = coursecode;
+this-> courseName = courseName;
+this-> maxStudents = maxStudents;
+this-> students = students;
+this-> currentStudents = currentStudents;
+}
+
+void addStudent(const Student &s){
+students+= &s;
+}
+
+void displayCourseInfo(){
+cout<<"Course name: <<courseName<<endl<<" Course code: "<<coursecode<<endl<<" Maximum students: "<<maxStudents<<endl<<
+"Current number of enrolled students: "<<currentstudents<<endl<<" Student's info: "<<students->display
+}
 
 
 
