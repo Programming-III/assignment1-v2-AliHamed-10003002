48e4fbe5948a6157de8936ee38e761c6cd7eb1e5
AliHamed-10003002 <ali.hamed@student.giu-uni.de>
2025-11-11 09:41:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,40 @@ using namespace std;
 
 
 // ==================== Course Class Implementation ====================
+class course{
+private:
+string coursecode;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
 
+public:
+course(string coursecode, string courseName, int maxStudents, Student* students, int currentStudents){
+this-> coursecode = coursecode;
+this-> courseName = courseName;
+this-> maxStudents = maxStudents;
+this-> students = students;
+this-> currentStudents = currentStudents;
+}
+course(){
+  coursecode = "";
+  courseName = "";
+  maxStudents = 0;
+  currentStudents = 0;
+}
+
+~course() {}
+
+void addStudent(const Student &s){
+students += &s;
+}
+
+ void displayCourseInfo(){
+  cout<<"Course name: <<courseName<<endl<<" Course code: "<<coursecode<<endl<<" Maximum students: "<<maxStudents<<endl<<
+  "Current number of enrolled students: "<<currentstudents<<endl<<" Student's info: "<<students->display()<<endl;
+}
+}
 
 
 
