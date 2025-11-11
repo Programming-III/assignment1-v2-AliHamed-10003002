277635eae0717c8d9c502b29237223a986171507
AliHamed-10003002 <ali.hamed@student.giu-uni.de>
2025-11-11 09:42:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,36 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+class Person{
+private:
+    string name;
+    int id;
+public:
+Person(string name, int id){
+    this->name = name;
+    this->id = id;
+}
+void setName(String n){
+name = n;
+}
+void setID(int x){
+    id = x;
+}
+
+string getName(){
+    return name;
+}
+
+int getID(){
+return id;
+}
+
+virtual void display(){
+    cout<<"Student name: "<<getName()<<"Student ID: "<<getID()<<endl;
+}
 
 
-
-
-
-
+}
 
 
 // ==================== Student Class Implementation ====================
