751e70eef4ad30895bfda5ebe0172b77bfc6ccb3
AliHamed-10003002 <ali.hamed@student.giu-uni.de>
2025-11-11 09:02:29 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,11 +1,23 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
+#include <iostream>
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
+class Student{
+private:
+int yearLevel;
+string major;
+public:
+Student(int yearLevel, string major){
+this-> yearLevel = yearLevel;
+this-> major = major; 
+}
 
+void display(){
+  cout<<"Student year lavel: "<<yearLevel<<"Student major: " <<major<<endl;
+}
+}
 
 
 
